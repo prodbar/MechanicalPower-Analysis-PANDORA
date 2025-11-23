3091b0a61c29fb9ee2bceefcef19419dd931dc82
prodbar <prodbar@etsii.upv.es>
2025-11-23 11:19:05 +0100

    Update plot_patient_MP_curves.m

--- src/matlab/plots/plot_patient_MP_curves.m
+++ src/matlab/plots/plot_patient_MP_curves.m
@@ -3,7 +3,7 @@
 % Plots individual mechanical power (MP) time curves for each patient,
 % using logarithmic scale on the X-axis (time in hours).
 %
-% Author: Patricia
+% Author: Patricia Rodrigo 
 % Repository: MechanicalPower-Analysis-PANDORA
 % --------------------------------------------------------------
 
